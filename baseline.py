c1a9f9f9a79698ad338821b164e653858298124f
jiwonii <wldnjs3633@naver.com>
2023-05-28 12:29:18 +0000

    fix) 시드 고정 함수 수정

--- baseline.py
+++ baseline.py
@@ -113,10 +113,12 @@ def set_seed(seed:int) -> None:
         args : seed(int) : seed 값
         returns : None
     '''
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
-    np.random.seed(seed)  
     random.seed(seed)  
+    np.random.seed(seed)  
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
